abf437fbe6114b469b18c23420b6adc7fc360261
scolphew <scolphew@pku.edu.cn>
2016-09-24 16:09:54 +0800

    100 判断二叉树相等 递归

---
+++ _100_SameTree.py
@@ -0,0 +1,28 @@
+from Tree import TreeNode
+
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        给定两颗二叉树，判断他们是否相等
+        结构相等，节点相等
+
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p is None and q is None:
+            return True
+        if p is None:
+            return False
+        if q is None:
+            return False
+        return p.val == q.val and self.isSameTree(p.left,
+                                                  q.left) and self.isSameTree(
+            p.right, q.right)
+
+
+p = TreeNode(0)
+q = TreeNode(0)
+s = Solution()
+print(s.isSameTree(p, q))
