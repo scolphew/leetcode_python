f5987e4d186c5e78c1c823b34f11c02f9d2d22dd
scolphew <scolphew@pku.edu.cn>
2016-09-24 15:44:32 +0800

    096 求有n个节点的二叉查找树的种类 DP num(n)=sum(num(i)*num(n-i-1) i=[0-n-1])

---
+++ _096_UniqueBinarySearchTrees.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        求有n个节点的二叉查找树的种类
+
+        :type n: int
+        :rtype: int
+        """
+        result = [1, 1, 2, 5]
+        if n < 4:
+            return result[n]
+
+        for i in range(4, n + 1):
+            result.append(sum(result[j] * result[i - j - 1] for j in range(i)))
+        return result[n]
+
+
+s = Solution()
+s.numTrees(6)
