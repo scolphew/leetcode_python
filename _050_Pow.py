4618e00339d29b7f1f7bda54879bac5e66067b25
scolphew <scolphew@pku.edu.cn>
2016-09-25 00:05:06 +0800

    050 乘方

---
+++ _050_Pow.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if x == 1:
+            return 1
+        if n == 1:
+            return x
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+
+        if not n % 2:
+            xx = self.myPow(x, n // 2)
+            return xx * xx
+        else:
+            return x * self.myPow(x, n - 1)
+
+
+s = Solution()
+print(s.myPow(8.88023, 3))
