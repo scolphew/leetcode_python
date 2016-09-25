0fc68f8e9bffb44d2288d7e2bcbd0b44b7398331
scolphew <scolphew@pku.edu.cn>
2016-09-25 23:05:17 +0800

    046 可以重复的元素的集合 求全排列
    两两交换位置
    ——相同的不交换
    ——每个元素只和start交换一次（第一次交换之后做标记）

---
+++ _047_PermutationsII.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        可以重复元素的集合， 求全排列
+
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not nums:
+            return [[]]
+        result = []
+
+        def perm(start=0, end=len(nums) - 1):
+            flag = set()
+            if start == end:
+                result.append(list(nums))
+            else:
+                perm(start + 1, end)
+                for i in range(start + 1, end + 1):
+                    if nums[start] != nums[i] and (start, nums[i]) not in flag:
+                        flag.add((start, nums[i]))
+                        nums[start], nums[i] = nums[i], nums[start]
+                        perm(start + 1, end)
+                        nums[start], nums[i] = nums[i], nums[start]
+
+        perm()
+        return result
+
+
+s = Solution()
+a = s.permuteUnique([1, 1, 2, 2, 3, 3])
+print(a)
+print(len(a))
