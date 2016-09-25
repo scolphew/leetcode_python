997ed68909ef542597cd92efc2874bca29d372ae
scolphew <scolphew@pku.edu.cn>
2016-09-25 21:07:06 +0800

    045 当前索引表示最大的跳数，求到达终点的最小跳数
    贪心
    寻找下一步的下一步能走到的最大距离，走下一步

--- _045_JumpGameII.py
+++ _045_JumpGameII.py
@@ -2,28 +2,36 @@ class Solution(object):
     def jump(self, nums):
         """
         当前索引表示最大的跳数，求到达终点的最小跳数
-
+        每一次挑选下一步能调的最远的地方
         :type nums: List[int]
         :rtype: int
         """
-        length = len(nums)
-        if length < 2:
-            return 0
-        if length == 2:
-            return 1
-        curIn = length - 1 # 剩余的长度（当前终点）（每次向前移动终点）
-        steps = 0 # 跳跃次数
-        while curIn > 0:
-            i = 0 # 每次从0开始
-            while i < curIn:
-                # 如果当前位置能跳到重点当前终点则前进一步
-                # 或者说把终点向前移动
-                if nums[i] >= curIn - i:
-                    steps += 1
-                    curIn = i
-                i += 1
-        return steps
+        step = last = next = 0
+        while next < len(nums) - 1:
+            step += 1
+            last, next = next, max(i + nums[i] for i in range(last, next + 1))
+        return step
+
+    def jump2(self, nums):
+        step = current = 0
+        while current < len(nums) - 1:
+            if current + nums[current] >= len(nums) - 1:
+                step += 1
+                break
+            else:
+                next = current + 1
+                nextNext = current + 2
+                jumpDist = nums[current]
+                for i in range(1, jumpDist + 1):
+                    temp = nums[current + i] + (current + i)
+                    if temp > nextNext:
+                        nextNext = temp
+                        next = current + i
+                current = next
+                step += 1
+        return step
 
 
 s = Solution()
 print(s.jump([2, 3, 1, 1, 4]))
+print(s.jump2([2, 3, 1, 1, 4]))
