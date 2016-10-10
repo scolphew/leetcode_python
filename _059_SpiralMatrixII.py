1b6f99998733b9849d08ca75edbabda9e9b6ddaa
scolphew <scolphew@pku.edu.cn>
2016-10-10 09:30:39 +0800

    059 螺旋矩阵 给一个数字，写出螺旋矩阵

---
+++ _059_SpiralMatrixII.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def generateMatrix(self, n):
+        """
+        螺旋矩阵
+        给一个数字，写出螺旋矩阵
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        result = [[0] * n for _ in range(n)]
+        elems = iter(range(1, n ** 2 + 1))
+        for i in range(n // 2 + 1):
+            for j in range(i, n - i):
+                result[i][j] = next(elems)
+            for j in range(i + 1, n - i):
+                result[j][n - i - 1] = next(elems)
+            for j in range(i + 1, n - i):
+                result[n - i - 1][n - j - 1] = next(elems)
+            for j in range(i + 1, n - i - 1):
+                result[n - j - 1][i] = next(elems)
+        print(result)
+
+
+s = Solution()
+s.generateMatrix(4)
+s.generateMatrix(5)
