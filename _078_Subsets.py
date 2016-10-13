aa0c4997436fd0aed27da0cf7193636e9f1ca07a
scolphew <scolphew@pku.edu.cn>
2016-10-13 19:15:01 +0800

    078 求子集

---
+++ _078_Subsets.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        求子集
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = [[]]
+        for i in nums:
+            x = [k for k in result]
+            for sub in x:
+                result.append(sub + [i])
+        return result
+
+
+s = Solution()
+print(s.subsets([1, 2]))
