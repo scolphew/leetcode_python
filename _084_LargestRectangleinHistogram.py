cbb6fbac9f50d819f27d283f678a44e33073e848
scolphew <scolphew@pku.edu.cn>
2016-10-19 23:02:52 +0800

    084 求直方图中最大矩形面积

---
+++ _084_LargestRectangleinHistogram.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        求直方图中最大矩形面积
+        :type heights: List[int]
+        :rtype: int
+        """
+        if not heights:
+            return 0
+        max_area = 0
+        heights.append(0)
+        i = 0
+        stack = []
+        while i < len(heights):
+            if not stack or heights[i] >= heights[stack[-1]]:
+                stack.append(i)
+                i += 1
+                continue
+            top_idx = stack.pop()
+            area = heights[top_idx] * (i if not stack else (i - stack[-1] - 1))
+            max_area = area if area > max_area else max_area
+        return max_area
+
+
+s = Solution()
+print(s.largestRectangleArea([2, 1, 5, 6, 2, 3]))
