5a1f25ae988ed676ab5ebda12fe5597db892c472
scolphew <scolphew@pku.edu.cn>
2016-10-22 15:55:09 +0800

    090 求nums的子集，nums可能含有重复元素

---
+++ _090_SubsetsII.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        求nums的子集，nums可能含有重复元素
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not nums:
+            return [[]]
+        result = [[]]
+        nums.sort()
+        size = 1
+        last = nums[0]
+        for i in nums:
+            if last != i:
+                last = i
+                size = len(result)
+            new_size = len(result)
+            for j in range(new_size - size, new_size):
+                result.append(result[j][:])
+                result[-1].append(i)
+        return result
+
+
+s = Solution()
+print(s.subsetsWithDup([1, 2, 2, 3, 3, 4]))
