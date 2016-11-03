e7a41ccaca30ddea5320a9170c034dc356711660
scolphew <scolphew@pku.edu.cn>
2016-11-03 23:16:02 +0800

    修改文件名的获取方方式 参数获取或者手动输入

--- excel/read_and_write.py
+++ excel/read_and_write.py
@@ -1,11 +1,20 @@
 import xlrd
 import xlsxwriter
+import sys
 
-v1_name = "aa.xlsx"  # 源文件名
-v2_name = 'bb.xlsx'  # 写入的文件名
-sheet_name = "abc"  # 源文件的sheet名字
-data = xlrd.open_workbook(v1_name)
-table = data.sheet_by_name(sheet_name)
+
+def input_file_name():
+    v1 = input("源文件名")
+    v2 = input("目标文件名")
+    sheet = input("工作表名")
+    return v1, v2, sheet
+
+
+def get_file_name():
+    if len(sys.argv) > 1:
+        return sys.argv[1], sys.argv[2], sys.argv[3]
+    else:
+        return input_file_name()
 
 
 def is_bad_case(key):
@@ -18,7 +27,10 @@ def is_bad_case(key):
 
 
 def copy_data():
-    workbook = xlsxwriter.Workbook('bb.xlsx')
+    v1_name, v2_name, sheet_name = get_file_name()
+    data = xlrd.open_workbook(v1_name)
+    table = data.sheet_by_name(sheet_name)
+    workbook = xlsxwriter.Workbook(v2_name)
     sheet = workbook.add_worksheet(sheet_name)
     index = 0
     for i in range(table.nrows):
@@ -27,6 +39,7 @@ def copy_data():
                 sheet.write(index, j, table.cell(i, j).value)
             index += 1
     workbook.close()
+
 
 if __name__ == '__main__':
     copy_data()
