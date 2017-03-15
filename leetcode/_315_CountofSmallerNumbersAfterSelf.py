76d3a1df96e75308e524d766a2a395f31ef0b12e
scolphew <scolphew@pku.edu.cn>
2017-03-15 12:29:04 +0800

    315 标出数组中在i后面小于i的数字的数量 分治 归并

--- leetcode/_315_CountofSmallerNumbersAfterSelf.py
+++ leetcode/_315_CountofSmallerNumbersAfterSelf.py
@@ -49,9 +49,46 @@ class Solution(object):
         print(root)
         return res
 
+    def countSmaller2(self, nums):
+        nums = [(j, 0, i) for i, j in enumerate(nums)]
+        length = len(nums)
+
+        def dc(l=0, r=length - 1):
+            if l >= r:
+                return
+            m = (l + r) >> 1
+            dc(l, m)
+            dc(m + 1, r)
+            i, j = l, m + 1
+            tmp_count = 0
+            index = 0
+            tmp_list = [0] * (r - l + 1)
+            while i <= m and j <= r:
+                # print(nums, i, j)
+                if nums[i][0] <= nums[j][0]:
+                    tmp_list[index] = (nums[i][0], nums[i][1] + tmp_count,
+                                       nums[i][2])
+                    i += 1
+                    index += 1
+                else:
+                    tmp_list[index] = nums[j]
+                    tmp_count += 1
+                    j += 1
+                    index += 1
+            if i > m:
+                tmp_list[index:] = nums[j:r + 1]
+            else:
+                tmp_list[index:] = [(a, b + tmp_count, c) for a, b, c in
+                                    nums[i: m + 1]]
+            nums[l:r + 1] = tmp_list
+
+        dc()
+        nums.sort(key=lambda x: x[2])
+        return [i[1] for i in nums]
+
 
 def p(s: Solution, input, output):
-    o = s.countSmaller(input)
+    o = s.countSmaller2(input)
     s = []
     for a, b in zip(o, output):
         if a == b:
