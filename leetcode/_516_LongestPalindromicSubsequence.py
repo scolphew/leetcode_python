78a2169c4401b07de6d5ed70dc76666d8c6fd9e0
scolphew <scolphew@pku.edu.cn>
2017-07-04 15:55:54 +0800

    516 最长回文子序列

--- leetcode/_516_LongestPalindromicSubsequence.py
+++ leetcode/_516_LongestPalindromicSubsequence.py
@@ -19,10 +19,26 @@ class Solution(object):
                 pro = tmp
         return dp[0]
 
+    def foo(self, s):
+        if s == s[::-1]:
+            return len(s)
+        else:
+            n = len(s)
+            dp = [[0] * n for _ in range(n)]
+            for i in range(n):
+                dp[i][i] = 1
+                for j in range(i - 1, -1, -1):
+                    if s[i] == s[j]:
+                        dp[j][i] = 2 + dp[j + 1][i - 1]
+                    else:
+                        dp[j][i] = max(dp[j + 1][i], dp[j][i - 1])
+            return dp[0][n - 1]
+
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.longestPalindromeSubseq("bbbab"))
+    print(s.foo("bbbab"))
     print(s.longestPalindromeSubseq(""))
+    print(s.longestPalindromeSubseq("aaaabbaaaa"))
     print(s.longestPalindromeSubseq("a"))
     print(s.longestPalindromeSubseq("aa"))
