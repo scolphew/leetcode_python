e1ba3ad98747d3d4c5c32e93adf81518c685bbf1
scolphew <scolphew@pku.edu.cn>
2017-07-16 13:05:34 +0800

    522 子序列2，求最长的子序列，某一个字符串的子序列不在不是其他的子序列

---
+++ leetcode/_522_LongestUncommonSubsequenceII.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findLUSlength(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: int
+        """
+
+        def issubsequence(s, t):
+            t = iter(t)
+            return all(c in t for c in s)
+
+        for s in sorted(strs, key=len, reverse=True):
+            if sum(issubsequence(s, t) for t in strs) == 1:
+                return len(s)
+        return -1
