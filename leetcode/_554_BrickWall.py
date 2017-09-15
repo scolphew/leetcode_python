2d534d330ead7c9a879dc17e35416d8de0b31483
lishaofei <lishaofei@imusics.net>
2017-09-15 20:52:10 +0800

    554

--- leetcode/_554_BrickWall.py
+++ leetcode/_554_BrickWall.py
@@ -9,21 +9,22 @@ class Solution(object):
         """
         from collections import defaultdict
         d = defaultdict(int)
-        for each in wall:
+        for each in wall[:-1]:
             left = 0
             for i in each:
                 left += i
-                d[left]+=1
+                d[left] += 1
         ans = 0
         for i in d.values():
             ans = max(ans, i)
         return len(wall) - ans
 
+
 if __name__ == '__main__':
     s = Solution()
     print s.leastBricks(
         [
-            [1,2,3],
-            [1,2,3],
+            [1, 2, 3],
+            [1, 2, 3],
         ]
     )
