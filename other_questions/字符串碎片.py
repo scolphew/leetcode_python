cc72ba730ac24068ddd62a9cb4b682e7367694a2
liyixuan <scolphew@pku.edu.cn>
2017-09-30 14:47:33 +0800

    啦啦啦

---
+++ other_questions/字符串碎片.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+s = input()
+if s:
+    lst = []
+    p = None
+    c = 0
+    for i in s:
+        if p == i:
+            c += 1
+        else:
+            lst.append(c)
+            p = i
+            c = 1
+    else:
+        lst.append(c)
+    ans = lst[1:]
+    ans = sum(lst) / len(ans)
+    print("%.2f" % round(ans, 2))
+else:
+    print(0)
