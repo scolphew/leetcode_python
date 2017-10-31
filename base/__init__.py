f8ec24178cd4e26b9d48f635d08b190b28932a56
liyixuan <scolphew@pku.edu.cn>
2017-10-31 11:33:58 +0800

    base init 添加直接import

--- base/__init__.py
+++ base/__init__.py
@@ -2,3 +2,20 @@
 基础包
 提供基本数据结构和基本算法的一些实现
 """
+from .LinkedList import ListNode
+from .interval import Interval
+from .Tree import TreeNode
+from .Tree import RedBlackTree
+from .Tree import SegmentTreeNode
+from .Tree import SegmentTree
+
+from .search import *
+from .sort import *
+
+from .base import run_time
+
+from .pmgressbar import ProgressBar
+
+__all__ = [
+    'search'
+]
