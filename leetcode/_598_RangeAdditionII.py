78f237c96a14fab07c0711acc0e49667d7151293
liyixuan <scolphew@pku.edu.cn>
2017-11-27 20:34:28 +0800

    m*n矩阵，一次在左上角的n组（x*y）个数+1，求最大的数的个数

---
+++ leetcode/_598_RangeAdditionII.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxCount(self, m, n, ops):
+        """
+        :type m: int
+        :type n: int
+        :type ops: List[List[int]]
+        :rtype: int
+        """
+        for i in ops:
+            m = min(m, i[0])
+            n = min(n, i[1])
+        return m * n
