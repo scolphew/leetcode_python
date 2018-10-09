da475c572e752beb0c4cef4c70aab094f6cf8546
scolphew <scolphew@pku.edu.cn>
2018-10-09 16:55:58 +0800

    685 树加一条边成为有向图，给出有向图找到一条边删掉之后成为树（多种情况删除给出的靠后面的边）

--- leetcode/_685_RedundantConnectionII.Solution.py
+++ leetcode/_685_RedundantConnectionII.Solution.py
@@ -1,52 +1,40 @@
 class Solution(object):
     def findRedundantDirectedConnection(self, edges):
-        import collections
-        N = len(edges)
-        parent = {}
-        candidates = []  # 两条边 相同的儿子
-        for u, v in edges:
-            if v in parent:
-                candidates.append((parent[v], v))
-                candidates.append((u, v))
+        from collections import defaultdict
+        parent_of = {}
+        candidates = []
+
+        for parent, child in edges:
+            if child in parent_of:
+                candidates.append([parent_of[child], child])
+                candidates.append([parent, child])
             else:
-                parent[v] = u
+                parent_of[child] = parent
 
-        def orbit(node):
-            """
-            寻找根节点
-            """
-            seen = set()
-            while node in parent and node not in seen:
-                seen.add(node)
-                node = parent[node]
-            return node, seen
-
-        root = orbit(1)[0]  # 根节点
-
-        # 如果 没有同一结点有两个父节点，说明存在环，删除环上任意边都可成树
-        if not candidates:
-            cycle = orbit(root)[1]  # 找到环上的任意结点
-            # 找到输入中最后一个环上的边，返回
-            for u, v in edges:
-                if u in cycle and v in cycle:
-                    ans = u, v
-            return ans
-
-        children = collections.defaultdict(list)
-        for v in parent:
-            children[parent[v]].append(v)
-
-        # 宽度优先遍历
-        seen = [True] + [False] * N
-        stack = [root]
-        while stack:
-            node = stack.pop()
-            if not seen[node]:
-                seen[node] = True
-                stack.extend(children[node])
-
-        # 如果能够遍历，则seen全为T，则输出后出现的那条便，否则，输出另一条
-        return candidates[all(seen)]
+        if candidates:
+            # 如果有一个结点有两个双亲
+            # 如果第一条边在环内，返回该边。否则返回另一条
+            u, v = candidates[0]
+            while u != v and u in parent_of:
+                u = parent_of[u]
+            # u==v 则有环（根不在环上） 返回前一个
+            # u!=v 无环 返回后一个
+            return candidates[u != v]
+        else:
+            # 有环，根在环上
+            s = set()
+            root = 1
+            while root in parent_of and root not in s:
+                # 有双亲且未访问
+                s.add(root)
+                root = parent_of[root]
+            s = set()
+            while root in parent_of and root not in s:
+                s.add(root)
+                root = parent_of[root]
+            for u, v in edges[::-1]:
+                if u in s and v in s:
+                    return [u, v]
 
 
 if __name__ == '__main__':
@@ -61,3 +49,7 @@ if __name__ == '__main__':
         [3, 1],
         [3, 5],
     ]))
+
+    print(s.findRedundantDirectedConnection(
+        [[1, 2], [2, 3], [3, 4], [4, 1], [1, 5]]
+    ))
