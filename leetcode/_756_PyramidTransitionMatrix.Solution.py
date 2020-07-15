f58caa119a26a33cfbd40c6c492645661f936f8e
scolphew <scolphew@pku.edu.cn>
2020-07-15 11:21:55 +0800

    756 递归1 现在，我们用一些方块来堆砌一个金字塔。 每个方块用仅包含一个字母的字符串表示。
    使用三元组表示金字塔的堆砌规则如下：
    
    对于三元组(A, B, C) ，“C”为顶层方块，方块“A”、“B”分别作为方块“C”下一层的的左、右子块。当且仅当(A, B, C)是被允许的三元组，我们才可以将其堆砌上。
    
    初始时，给定金字塔的基层 bottom，用一个字符串表示。一个允许的三元组列表 allowed，每个三元组用一个长度为 3 的字符串表示。
    
    如果可以由基层一直堆到塔尖就返回 true ，否则返回 false 。

---
+++ leetcode/_756_PyramidTransitionMatrix.Solution.py
@@ -0,0 +1,54 @@
+from typing import List
+
+
+class Solution:
+    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        from collections import defaultdict
+        d = defaultdict(set)
+        for a, b, c in allowed:
+            d[a, b].add(c)
+
+        def dfs(A):
+            level = len(A)
+            if level == 1:
+                return True
+            this = [None] * (level - 1)
+            i, j = 0, 0
+            while i < level - 1:  # 新层第i位
+                while j < 7:  # ABCDEFG
+                    if chr(65 + j) in d[A[i], A[i + 1]]:  # 能放
+                        this[i] = chr(65 + j)
+                        j = 0
+                        break
+                    else:
+                        j += 1
+
+                if this[i] is None:  # 没什么可放的
+                    if i == 0:
+                        break
+                    else:
+                        i -= 1
+                        j = ord(this[i]) - 65 + 1
+                        this[i] = None
+                        continue
+                if i == level - 2:
+                    if dfs(this):
+                        return True
+                    else:
+                        j = ord(this[i]) - 65 + 1
+                        this[i] = None
+                        continue
+                i += 1
+            return False
+
+        return dfs(bottom)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.pyramidTransition("CCC",
+                              ["CBB", "ACB", "ABD", "CDB", "BDC", "CBC", "DBA",
+                               "DBB", "CAB", "BCB", "BCC", "BAA", "CCD", "BDD",
+                               "DDD", "CCA", "CAA", "CCC", "CCB"]))
+    print(s.pyramidTransition("AABA", ["AAA", "AAB", "ABA", "ABB", "BAC"]))
+    print(s.pyramidTransition("BCD", ["BCG", "CDE", "GEA", "FFF"]))
