252046b5331f14d6d8d87e5f1f1fdb4ea2b8a46c
李懿轩 <scolphew@outlook.com>
2022-06-10 10:19:54 +0800

    825

---
+++ leetcode/_825_FriendsOfAppropriateAges.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def numFriendRequests(self, ages: List[int]) -> int: # noqa
+        cnt = [0] * 121
+        for age in ages:
+            cnt[age] += 1
+        pre = [0] * 121
+        for i in range(1, 121):
+            pre[i] = pre[i - 1] + cnt[i]
+
+        ans = 0
+        for i in range(15, 121):
+            if cnt[i] > 0:
+                bound = int(i * 0.5 + 8)
+                ans += cnt[i] * (pre[i] - pre[bound - 1] - 1)
+        return ans
+
